53c5f793600f730d42aae033dd767612e52b944d
青云依兮 <1422756921@qq.com>
2023-04-14 19:10:17 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+console.log("%c青云依兮", "color: #6d85c1; font-size: 2.5em;");
+var audio = document.getElementById('audio');
+audio.play();
+var lis = document.querySelectorAll('ul.jj li');
+lis.forEach(li => {
+    li.onclick = function() {
+        audio.src = './res/' + this.getAttribute('m') + '.mp3';
+        audio.play();
+        console.log(this.innerHTML)
+    }
+});
+document.querySelector('.s1').onclick = function() {
+    audio.currentTime = 0;
+    console.log('重开')
+};
+document.querySelector('.s2').onclick = function() {
+    audio.pause();
+    console.log('停止')
+}
